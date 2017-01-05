75bdb29e304909e577d1d4d3ebeea86bdb06834d
zym <eyeder@163.com>
2017-01-06 02:10:12 +0800

    dropdown add setTimeout function

--- src/SubMenu.jsx
+++ src/SubMenu.jsx
@@ -2,9 +2,39 @@ import React from 'react';
 import SubPopupMenu from './SubPopupMenu.jsx';
 
 export default class SubMenu extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			active: false,
+			selected: false
+		}
+	}
+
+	handleHover(){
+		clearTimeout(this.timer);
+		this.setState({
+			active: true
+		})
+	}
+
+	handleLeave(){
+		var that = this;
+		this.timer = setTimeout(function(){
+			that.setState({
+				active: false
+			});
+		}, 300);
+	}
+
 	render(){
+		var actived = this.state.active ? "submenu-active " : "";
+		var selected = this.state.selected ? "submenu-selected " : "";
+		var classNames = "menu-item " + actived + selected;
 		return(
-			<li className="menu-item">
+			<li className={classNames} 
+				onMouseOver={this.handleHover.bind(this)} 
+				onMouseLeave={this.handleLeave.bind(this)} >
 				{this.props.children}
 			</li>
 		);
--- src/SubPopupMenu.jsx
+++ src/SubPopupMenu.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import MenuItemGroup from './MenuItemGroup.jsx';
 
 export default class SubPopupMenu extends React.Component {
+
+	constructor(props){
+		super(props);
+	}
+
+
+
 	render(){
 		return(
-			<ul className="submenu">
+			<ul className="submenu" >
 				{this.props.children}
 			</ul>
 		);
